7ddc06c1a795b7cb149104b7cc6a735fc6ac02c3
ghul7 <mbotwina@gmial.com>
2022-06-10 19:58:56 +0200

    dodanie nowego projektu

---
+++ src/main/java/pl/sdacademy/unit/test/advance/examples/exceptions/Projekt.java
@@ -0,0 +1,8 @@
+package pl.sdacademy.unit.test.advance.examples.exceptions;
+
+public class Projekt {
+
+    public static void main(String[] args) {
+        System.out.println("ala ma kota");
+    }
+}
