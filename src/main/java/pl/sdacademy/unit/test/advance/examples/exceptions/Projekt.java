dd00c2589cf2aeefa8e244d2cd7c3d726b45060f
ghul7 <mbotwina@gmial.com>
2022-06-10 22:19:48 +0200

    ponowne dodanie zmian

--- src/main/java/pl/sdacademy/unit/test/advance/examples/exceptions/Projekt.java
+++ src/main/java/pl/sdacademy/unit/test/advance/examples/exceptions/Projekt.java
@@ -5,7 +5,6 @@ public class Projekt {
     public static void main(String[] args) {
         System.out.println("ala ma kota");
 
-
-        System.out.println("teraz dodaje zmiany");
+        
     }
 }
